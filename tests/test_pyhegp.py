402a99a400cc5bfdd27349e8e84d54f46646e78c
Arun Isaac <arunisaac@systemreboot.net>
2025-07-15 16:39:44 +0100

    Use default array shapes testing encryption/decryption.
    It may be better to sample a smaller set of matrices finely than a
    large set of matrices coarsely.
    
    * tests/test_pyhegp.py (test_hegp_encryption_decryption_are_inverses):
    Use default array shapes testing encryption/decryption.

--- tests/test_pyhegp.py
+++ tests/test_pyhegp.py
@@ -25,18 +25,17 @@ from pyhegp.pyhegp import hegp_encrypt, hegp_decrypt, random_key
 
 @given(st.one_of(
     arrays("int32",
-           array_shapes(min_dims=2, max_dims=2, min_side=2, max_side=100),
+           array_shapes(min_dims=2, max_dims=2),
            elements=st.integers(min_value=0, max_value=2)),
     # The array above is the only realistic input, but we test more
     # kinds of inputs for good measure.
     arrays("int32",
-           array_shapes(min_dims=2, max_dims=2, min_side=2, max_side=100),
+           array_shapes(min_dims=2, max_dims=2),
            elements=st.integers(min_value=0, max_value=100)),
     arrays("float64",
-           array_shapes(min_dims=2, max_dims=2, min_side=2, max_side=100),
+           array_shapes(min_dims=2, max_dims=2),
            elements=st.floats(min_value=0, max_value=100)))
 )
-@settings(deadline=None)
 def test_hegp_encryption_decryption_are_inverses(plaintext):
     rng = np.random.default_rng()
     key = random_key(rng, len(plaintext))
